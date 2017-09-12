3034606e74cd3ad46a4247dd9744390138f4ce51
Renee Bialas <rbialas@umail.iu.edu>
2017-09-11 21:54:39 -0400

    Implemented BFS and general search

--- hw_1.py
+++ hw_1.py
@@ -1,6 +1,9 @@
 import random
 import copy
 
+board = []
+explored = []
+
 # Creates a goal board based on a given size
 # Takes an int called size
 # Returns a goal board of given size
@@ -11,90 +14,111 @@ def makeGoalBoard(size):
         nRow = []
         for y in range(size):
             if(num != size*size):
-                nRow.append(num)   
+                nRow.append(num)
                 num += 1
             else:
                 nRow.append("")
         nBoard.append(nRow)
     return(nBoard)
 
-#Stores info about puzzles    
-class Puzzle:
-    size = 0
-    board = []
+#Prints a puzzle
+def makeState(board):
+    for row in board:
+        print(row)
 
-    #Constructor
-    def __init__(self, b):
-        self.size = len(b)
-        self.board = b
-        
-    #Prints a puzzle
-    def makeState(self):
-        for row in self.board:
-            print(row)
-    
-    # returns a board after a move is made
-    # Takes a puzzle and move in the form (newSpaceRow, newSpaceCol, oldSpaceRow, oldSpaceCol)
-    # Returns a board with the given move made
-    def makeMove(self, move):
-        newBoard = copy.deepcopy(self.board)
-        newBoard[move[0]][move[1]] = "";
-        newBoard[move[2]][move[3]] = self.board[move[0]][move[1]]
-        return(newBoard)
-    
-    # generates all possible moves given a board
-    # Returns a list of moves
-    # A move is a tuple of the shape (newSpaceRow, newSpaceCol, oldSpaceRow, oldSpaceCol)
-    def possMoves(self):
-        for x in range(len(self.board)):
-            if "" in self.board[x]:
-                oldSpaceRow = x;
-                spaceIndCol = self.board[x].index("")
-                oldSpaceCol = spaceIndCol
-                newPossInds = []
-                if(oldSpaceRow - 1 >= 0):
-                    newPossInds.append((oldSpaceRow-1, oldSpaceCol, oldSpaceRow, oldSpaceCol))
-                if(oldSpaceRow + 1 < self.size):
-                    newPossInds.append((oldSpaceRow+1, oldSpaceCol, oldSpaceRow, oldSpaceCol))
-                if(oldSpaceCol - 1 >= 0):
-                    newPossInds.append((oldSpaceRow, oldSpaceCol - 1, oldSpaceRow, oldSpaceCol))
-                if(oldSpaceCol + 1 < self.size):
-                    newPossInds.append((oldSpaceRow, oldSpaceCol + 1, oldSpaceRow, oldSpaceCol))
-                return(newPossInds)
-                break
-            
-    # Randomizes a puzzle from whatever state it's already in
-    def randomBoard(self):
-        numMoves = random.randrange(1,self.size * 1000)
-        for x in range(numMoves):
-            nextMoves = self.possMoves()
-            moveInd = random.randrange(0,len(nextMoves))
-            self.board = self.makeMove(nextMoves[moveInd])
+# returns a board after a move is made
+# Takes a puzzle and move in the form (newSpaceRow, newSpaceCol, oldSpaceRow, oldSpaceCol)
+# Returns a board with the given move made
+def makeMove(board, move):
+     newBoard = copy.deepcopy(board)
+     newBoard[move[0]][move[1]] = "";
+     newBoard[move[2]][move[3]] = board[move[0]][move[1]]
+     return(newBoard)
+
+# generates all possible moves given a board
+# Returns a list of moves
+# A move is a tuple of the shape (newSpaceRow, newSpaceCol, oldSpaceRow, oldSpaceCol)
+def possMoves(board):
+     for x in range(len(board)):
+         if "" in board[x]:
+            size = len(board);
+            oldSpaceRow = x;
+            spaceIndCol = board[x].index("")
+            oldSpaceCol = spaceIndCol
+            newPossInds = []
+            if(oldSpaceRow - 1 >= 0):
+                newPossInds.append((oldSpaceRow-1, oldSpaceCol, oldSpaceRow, oldSpaceCol))
+            if(oldSpaceRow + 1 < size):
+                newPossInds.append((oldSpaceRow+1, oldSpaceCol, oldSpaceRow, oldSpaceCol))
+            if(oldSpaceCol - 1 >= 0):
+                newPossInds.append((oldSpaceRow, oldSpaceCol - 1, oldSpaceRow, oldSpaceCol))
+            if(oldSpaceCol + 1 < size):
+                newPossInds.append((oldSpaceRow, oldSpaceCol + 1, oldSpaceRow, oldSpaceCol))
+            return(newPossInds)
+            break
+
+# Randomizes a puzzle from whatever state it's already in
+def randomBoard(board):
+    randBoard = copy.deepcopy(board)
+    numMoves = random.randrange(0, 19)
+    for x in range(numMoves):
+        nextPossMoves = possMoves(randBoard)
+        nextMoveInt = random.randrange(0, len(nextPossMoves))
+        randBoard = copy.deepcopy(makeMove(randBoard, nextPossMoves[nextMoveInt]))
+    return randBoard
+
+def testProcedure(currentNode):
+    return currentNode[0] == makeGoalBoard(len(currentNode[0]))
+
+def outputProcedure(numruns, currentNode):
+    while (numruns >= 0 and currentNode is not None):
+        makeState(currentNode[0])
+        print()
+        currentNode = currentNode[1]
+        numruns -= 1
+
+def expandProcedure(currentNode, queue, explored):
+    possibleMoves = possMoves(currentNode[0])
+    possibleMovesMade = []
+    nqueue = copy.deepcopy(queue)
+
+    for move in possibleMoves:
+        possibleMovesMade.append(makeMove(currentNode[0], move))
+    for possibleMoveMade in possibleMovesMade:
+        if (possibleMoveMade not in explored):
+            nqueue.append(makeNode(possibleMoveMade, currentNode, currentNode[2] + 1, currentNode[3]))
+    return nqueue
 
 def makeNode(state, parent, depth, pathCost):
     return [state, parent, depth, pathCost]
 
 def generalSearch(queue, limit, numRuns):
+    print(limit)
+    node = queue[0]
+    makeState(node[0])
     if queue == []:
         return False
     elif testProcedure(queue[0]):
+        print("OUTPUT")
         outputProcedure(numRuns, queue[0])
     elif limit == 0:
         print("Limit reached")
     else:
         limit -= 1
         numRuns += 1
-        generalSearch(expandProcedure(queue[0], queue[1:len(queue)]), limit, numRuns)
+        node = queue[0]
+        explored.append(node[0])
+        generalSearch(expandProcedure(queue[0], queue[1:len(queue)], explored), limit, numRuns)
 
-p1 = Puzzle(makeGoalBoard(3))
-p1.makeState()
-print()
-p1.randomBoard()
-p1.makeState()
-print()
+#p1 = makeGoalBoard(3)
+#makeState(p1)
+#print()
+#randomBoard(p1)
+#makeState(p1)
+#print()
 
-p2 = Puzzle(makeGoalBoard(5))
-p2.makeState()
-print()
-p2.randomBoard()
-p2.makeState()
+start = randomBoard(makeGoalBoard(3))
+startNode = makeNode(start, None, 1, 0)
+generalSearch([startNode], 100, 0)
+
+
